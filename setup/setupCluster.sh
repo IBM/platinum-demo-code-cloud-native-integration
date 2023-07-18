22a951601d10935d28223df9a7a2cbab2cbbc09d
Callum Jackson <callumj@uk.ibm.com>
2023-07-18 14:10:44 +0100

    Cluster setup update

--- setup/setupCluster.sh
+++ setup/setupCluster.sh
@@ -20,22 +20,6 @@ kubectl patch storageclass ocs-storagecluster-ceph-rbd -p '{"metadata": {"annota
 
 $SCRIPT_DIR/installPipelineOnly.sh
 
-#Get currentSecret
-#oc get secret -n openshift-config pull-secret -o "jsonpath={.data.\.dockerconfigjson}" | base64 -d > ORIGINAL_SECRETS
-#cat ORIGINAL_SECRETS
-
-#COUNT_ICR_SECRETS=$(cat ORIGINAL_SECRETS | jq '.auths."cp.icr.io" | length')
-
-#if (( COUNT_ICR_SECRETS == 0 )); then
-#  echo "Updating Secret to add IBM Entitlement Key"
-
-  #UPDATE_SECRET=$(jq '.auths |= . + {"cp.icr.io": {"username": "cp", "password": "'$ENTITLED_REG_KEY'", "auth":"'$ENCODING'", "email": "callumj@uk.ibm.com"}}' ORIGINAL_SECRETS | base64 -w 0) 
-
-  #oc patch secret pull-secret -p '{"data": {".dockerconfigjson": "'$UPDATE_SECRET'"}}' -n openshift-config --type=merge
-
-#fi
-
-#rm ORIGINAL_SECRETS
 oc new-project student1
 oc new-project student2
 oc new-project student3
